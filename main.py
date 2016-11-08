9080c3790f233f9dc2efd849fb5a290c6357c6dc
Rafahel <rafahelmello@gmail.com>
2016-11-08 16:48:24 -0200

    Estados existentes criados

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ def main():
     linhas = arquivo.readlines()
     cabecalho = linhas[0]
     linhas.pop(0)
-    novosEstados = []
     automato = []
     for elemento in linhas:
         automato.append(elemento.split())
     tam = automato[0]
-    print(len(tam))
+    # print(len(tam))
     for i in range(len(automato)):
         for j in range(len(tam)):
-            print(automato[i][j])
+            # print(automato[i][j])
             if "," in automato[i][j]:
                 novoEstado = automato[i][j]
                 novoEstado = novoEstado.replace(",", "")
@@ -20,12 +19,18 @@ def main():
     #
     # for elemento in automato:
     #     print(elemento)
-
+    novoAutomato = []
+    novoAutomato.append(automato[0])
+    estadosExistentes = []
+    estadosExistentes.append(automato[0][0])
+    # print(novoAutomato)
+    novosEstados = []
     for i in range(len(automato)):
         for j in range(len(tam)):
-            print(automato[i][j])
-            if "," in automato[i][j]:
-               print(automato[i][j])
+            print(automato[i][0])
+            if automato[i][0] not in estadosExistentes:
+                estadosExistentes.append(automato[i][0])
+    print(estadosExistentes)
 
 
 
