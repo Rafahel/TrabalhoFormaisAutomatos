19c8e8e91006b6ca5f6cd2f9896cf810118080a2
Rafahel <rafahelmello@gmail.com>
2016-11-08 16:56:20 -0200

    Comentários

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
         automato.append(elemento.split())
     tam = automato[0]
     # print(len(tam))
+    '''
+        Cria uma lista [vetor] com as linhas e colunas do automato eliminando a linha 1 que é os simbolos a serem lidos
+    '''
     for i in range(len(automato)):
         for j in range(len(tam)):
             # print(automato[i][j])
@@ -16,21 +19,23 @@ def main():
                 novoEstado = automato[i][j]
                 novoEstado = novoEstado.replace(",", "")
                 automato[i][j] = novoEstado
-    #
-    # for elemento in automato:
-    #     print(elemento)
     novoAutomato = []
     novoAutomato.append(automato[0])
     estadosExistentes = []
     estadosExistentes.append(automato[0][0])
     # print(novoAutomato)
     novosEstados = []
+    '''
+        Cria uma lista com os estados existentes
+    '''
     for i in range(len(automato)):
         for j in range(len(tam)):
-            print(automato[i][0])
+            # print(automato[i][0])
             if automato[i][0] not in estadosExistentes:
                 estadosExistentes.append(automato[i][0])
-    print(estadosExistentes)
+    # print(estadosExistentes)
+    for elemento in automato:
+        print(elemento)
 
 
 
