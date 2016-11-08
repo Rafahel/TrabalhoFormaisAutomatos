d3b8fe367f58c39e28d85763ebd3a203ac18c0fd
Rafahel <rafahelmello@gmail.com>
2016-11-08 15:54:58 -0200

    ...

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 
 def main():
-    arquivo = open("AUT", "r")
+    arquivo = open("aut", "r")
     linhas = arquivo.readlines()
-    print(linhas)
+    cabecalho = linhas[0]
+    linhas.pop(0)
+    novosEstados = []
+    automato = []
+    for elemento in linhas:
+        automato.append(elemento.split())
+    tam = automato[0]
+    print(len(tam))
+    for i in range(len(automato)):
+        for j in range(len(tam)):
+            print(automato[i][j])
+            if "," in automato[i][j]:
+                novoEstado = automato[i][j]
+                novoEstado = novoEstado.replace(",", "")
+                automato[i][j] = novoEstado
+
+    for elemento in automato:
+        print(elemento)
+
+    for i in range(len(automato)):
+        for j in range(len(tam)):
+            print(automato[i][j])
+            if "," in automato[i][j]:
+               print(automato[i][j])
+
+
+
+
 
 
 
